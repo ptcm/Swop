4e1f4180196bc3038ddde904bb2390b70864a0a8
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-10-15 14:28:29 +0200

    changing page style

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,22 @@
 <!DOCTYPE = html>
 <html>
   <head>
+	   <style>
+        p {
+            font-size: 20px;
+            font-weight: bold;
+        }
+        h1 {
+            font-size: 90px;
+            color: firebrick;
+        }h2 {
+            font-size: 50px;
+            color: purple;
+        }
+        header {
+            background-color: greenyellow;
+        }
+    </style>
   <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
