5d3a431bf98774e0d62a6c91368caaee5ba6071c
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-10-17 22:51:40 +0200

    Add files via upload
    js file for welcome note

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+alert("Hello from Partson & Lovejoy, This site is underconstruction");
+alert("we are happy to have you at this Site, Now you can go through the site!");
+document.write("SwopMatch Handler");
+document.wrong("<p>I'm practicing 'debugging'.</p>");
+
+
+
