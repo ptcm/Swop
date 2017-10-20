545101b2c3694d59cdfe93bcf1d6ead024aacc42
ptcm <32480753+ptcm@users.noreply.github.com>
2017-10-20 13:50:01 +0200

    Update reports.php
    changes made on reports home mainly do to with styling

--- reports.php
+++ reports.php
@@ -10,43 +10,40 @@ include("inc/header.php"); ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
 	<title><?php echo $pageTitle ?></title>
-	<link rel = "stylesheet" href = "css/style.css"> 
+	<link rel = "stylesheet" href = "css/styles.css"> 
   </head>
 	<div class="form">
 		<h2>Please select a report below</h2>
 		
-		<table>
-		<tr>
-				<td colspan = 4><a href="clients_list.php" target = "_blank">Clients List</a></td>
+		<table class = "table">
+			<tr>
+				<td><a href="clients_list.php" target = "_blank" class = "prefButtons">Clients List</a></td>
+        <td><a href="current_schools_list.php" target = "_blank"  class = "prefButtons">Current Schools List</a></td>
+				<td><a href="preferred_provinces_list.php" target = "_blank" class = "prefButtons">Preferred Provinces List</a></td>
+				<td><a href="preferred_districts_list.php" target = "_blank" class = "prefButtons">Preferred Districts1 List</a></td>
 			</tr>
 			<tr>
-				<td><a href="current_schools_list.php" target = "_blank">Current Schools List</a></td>
-				<td><a href="preferred_provinces_list.php" target = "_blank">Preferred Provinces List</a></td>
-				<td><a href="preferred_districts_list.php" target = "_blank">Preferred Districts1 List</a></td>
-        <td><a href="preferred_districts2_list.php" target = "_blank">Preferred Districts2 List</a></td>
-			</tr>
-			<tr>
-				<td><a href="preferred_towns_list.php" target = "_blank">Preferred Towns List</a></td>
-				<td><a href="preferred_locations_list.php" target = "_blank">Preferred Locations1 List</a></td>
-        <td><a href="preferred_locations2_list.php" target = "_blank">Preferred Locations2 List</a></td>
-        <td><a href="preferred_locations3_list.php" target = "_blank">Preferred Locations3 List</a></td>
+        <td><a href="preferred_districts2_list.php" target = "_blank" class = "prefButtons">Preferred Districts2 List</a></td>
+				<td><a href="preferred_towns_list.php" target = "_blank" class = "prefButtons">Preferred Towns List</a></td>
+				<td><a href="preferred_locations_list.php" target = "_blank" class = "prefButtons">Preferred Locations1 List</a></td>
+        <td><a href="preferred_locations2_list.php" target = "_blank" class = "prefButtons">Preferred Locations2 List</a></td>
 			</tr>
       <tr>
-				<td><a href="preferred_schools_list.php" target = "_blank">Preferred schools1 List</a></td>
-				<td><a href="preferred_schools2_list.php" target = "_blank">Preferred schools2 List</a></td>
-        <td><a href="preferred_schools3_list.php" target = "_blank">Preferred schools3 List</a></td>
-        <td><a href="preferred_schools4_list.php" target = "_blank">Preferred schools4 List</a></td>
+        <td><a href="preferred_locations3_list.php" target = "_blank" class = "prefButtons">Preferred Locations3 List</a></td>
+				<td><a href="preferred_schools_list.php" target = "_blank" class = "prefButtons">Preferred schools1 List</a></td>
+				<td><a href="preferred_schools2_list.php" target = "_blank" class = "prefButtons">Preferred schools2 List</a></td>
+        <td><a href="preferred_schools3_list.php" target = "_blank" class = "prefButtons">Preferred schools3 List</a></td>
 			</tr>
        <tr>
-				<td><a href="preferred_schools5_list.php" target = "_blank">Preferred schools5 List</a></td>
-				<td><a href="preferred_schools6_list.php" target = "_blank">Preferred schools6 List</a></td>
-        <td><a href="preferred_schools7_list.php" target = "_blank">Preferred schools7 List</a></td>
-        <td><a href="preferred_schools8_list.php" target = "_blank">Preferred schools8 List</a></td>
+        <td><a href="preferred_schools4_list.php" target = "_blank" class = "prefButtons">Preferred schools4 List</a></td>
+				<td><a href="preferred_schools5_list.php" target = "_blank" class = "prefButtons">Preferred schools5 List</a></td>
+				<td><a href="preferred_schools6_list.php" target = "_blank" class = "prefButtons">Preferred schools6 List</a></td>
+        <td><a href="preferred_schools7_list.php" target = "_blank" class = "prefButtons">Preferred schools7 List</a></td>
 			</tr>
       <tr>
-				<td><a href="preferred_schools9_list.php" target = "_blank">Preferred schools9 List</a></td>
-				<td><a href="preferred_schools10_list.php" target = "_blank">Preferred schools10 List</a></td>
-        <td></td>
+        <td><a href="preferred_schools8_list.php" target = "_blank" class = "prefButtons">Preferred schools8 List</a></td>
+				<td><a href="preferred_schools9_list.php" target = "_blank" class = "prefButtons">Preferred schools9 List</a></td>
+				<td><a href="preferred_schools10_list.php" target = "_blank" class = "prefButtons">Preferred schools10 List</a></td>
         <td></td>
 			</tr>
 		</table>
