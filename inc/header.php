5811006de3e4d0d77bece6d82f1c7c741ebee353
ptcm <32480753+ptcm@users.noreply.github.com>
2017-10-20 14:17:06 +0200

    Update header.php
    updated

--- inc/header.php
+++ inc/header.php
@@ -1,22 +1,6 @@
 <!DOCTYPE = html>
 <html>
   <head>
-	   <style>
-        p {
-            font-size: 20px;
-            font-weight: bold;
-        }
-        h1 {
-            font-size: 90px;
-            color: firebrick;
-        }h2 {
-            font-size: 50px;
-            color: purple;
-        }
-        header {
-            background-color: greenyellow;
-        }
-    </style>
   <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
@@ -25,11 +9,11 @@
 	<link rel = "stylesheet" href = "../css/styles.css"> 
   </head>
 
-  <body>
+  <body class = "body">
     <div class = "section hidden">
 		<header>
 		  <h1 id="sitename">SwopMatch Handler</h1>
-			<nav class = "menu" id="mainlinks">
+		<!--	<nav class = "menu" id="mainlinks"> -->
 			      <ul>
 					<li><a href="index.php">Home</a></li>
 					<li><a href="Account_manage.php" target = "_blank">My Account</a></li>
@@ -37,6 +21,6 @@
 					<li><a href="reports.php" target = "_blank">Reports</a></li>
 					<li><a href="#">Contact</a></li>
 				</ul>
-			 </nav>
+			<!-- </nav> -->
 		</header>
 	</div>
