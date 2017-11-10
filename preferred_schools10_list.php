7e62fddd5eab656bb924e57f163423bec55e3c52
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-10 12:59:47 +0200

    Update preferred_schools10_list.php

--- preferred_schools10_list.php
+++ preferred_schools10_list.php
@@ -21,11 +21,12 @@ if(isset($error_message)){
 ?>
 <html>
 	<body>
-		<table>
+		<table class="table table-responsive-sm table-warning table-hover>
 			<tr>
-				<th>Record Id</th>
-				<th>EC Number</th>
-				<th>School Name</th>
+			    <th scope="col">#</th>
+				<th scope="col">Record Id</th>
+				<th scope="col">EC Number</th>
+				<th scope="col">School Name</th>
 				</tr>
 			<?php
 			foreach (get_pref_schools10_list() as $item){
@@ -34,7 +35,7 @@ if(isset($error_message)){
 				'<td>'.ucwords(strtolower($item['school_name'])).'</td>';
         echo '<td><form method="post" action="preferred_schools10_list.php" onsubmit="return confirm(\'Are you sure you want to delete this school?\')">';
         echo '<input type="hidden" value="'.$item['mps10_id'].'" name="delete"/>';
-        echo '<input type="submit" value="Delete"/>';
+        echo '<input type="submit" class="btn btn-outline-danger" value="Delete"/>';
         echo '</form>';
         echo '</td></tr>';
 			}
