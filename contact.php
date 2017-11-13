ac35a121c35b9bb87604440a8f433372545db4bd
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-13 23:28:22 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,41 @@
+<?php
+$pageTitle = "| Home";
+include("inc/header.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $pageTitle ?></title>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script type="text/javascript" src="js/script.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<!--contact-->
+<div class="jumbotron alert-danger clearfix class="border border-primary">
+    <div class="card-header">Contact</div>
+    <div class="container w-50 p-3 pt-5">
+
+        <p class="card-text">
+        <p>for more info you may contact!!</p>
+        <p><strong>Email:</strong><a href="mailto:pchizema@gmail.com>?subject=if you have issues I  will be to assist you!">pchizema@gmail.com</a></p>
+        <p><strong>Phone:</strong>+263 712549887</p>
+        <p><strong>Email:</strong> <a href="mailto:maluwalovejoy2@gmail.com?subject=Hi There, you are free to ask anything!">maluwalovejoy2@gmail.com</a></p>
+        <p><strong>Phone:</strong>+263 778846005</p>
+        <footer>
+            <p><small>&copy; SwopMatch Handler</small></p>
+        </footer></p>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        </body>
+</html>
+<?php include("inc/footer.php"); ?>
