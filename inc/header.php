ae5b0017c2257202aa630810a7b2f2e14abd43c1
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-14 15:49:14 +0200

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,26 +1,23 @@
-<!DOCTYPE = html>
-<html>
-  <head>
-  <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="../csss/main.css">
-    <title><?php $pageTitle ?></title>
-	<link rel = "stylesheet" href = "../css/styles.css"> 
-  </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SwopMatch Handler</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body id="home" data-spy="scroll" data-target=".navbar" data-offset="100">
 
-  <body class = "body">
-    <div class = "section hidden">
-		<header>
-		  <h1 id="sitename">SwopMatch Handler</h1>
-		<!--	<nav class = "menu" id="mainlinks"> -->
-			      <ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="Account_manage.php" target = "_blank">My Account</a></li>
-					<li><a href="suggest.php" target = "_blank">Suggest</a></li>
-					<li><a href="reports.php" target = "_blank">Reports</a></li>
-					<li><a href="#">Contact</a></li>
-				</ul>
-			<!-- </nav> -->
-		</header>
-	</div>
+<!-- navbar -->
+
+<nav class="nav nav-pills flex-column flex-sm-row">
+    <a class="flex-sm-fill text-sm-center nav-link active" href="#">Home</a>
+    <a class="flex-sm-fill text-sm-center nav-link active" href="Account_manage.php" target = "_blank">My Account</a>
+    <a class="flex-sm-fill text-sm-center nav-link active" href="suggest.php" target = "_blank">Suggest</a>
+    <a class="flex-sm-fill text-sm-center nav-link active" href="reports.php" target = "_blank">Reports</a>
+    <a class="flex-sm-fill text-sm-center nav-link active" href="contact.php" target = "_blank">Contact</a>
+</nav>
+<!-- /navbar -->
