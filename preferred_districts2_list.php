91e1adcf37f7f7965071297100c554598e4e2b6f
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-14 17:40:18 +0200

    Update preferred_districts2_list.php

--- preferred_districts2_list.php
+++ preferred_districts2_list.php
@@ -23,13 +23,11 @@ if(isset($error_message)){
 	<body>
 		<table class="table table-responsive-sm table-warning table-hover">
 
-
 			<tr>
-                <th scope="col">#</th>
-                <th scope="" $().button('toggle')
-				<th scope="col">Record Id</th>
-				<th scope="col">EC Number</th>
-				<th scope="col">District Name</th>
+
+				<th>Record Id</th>
+				<th>EC Number</th>
+				<th>District Name</th>
 				</tr>
 			<?php
 			foreach (get_pref_districts2_list() as $item){
@@ -38,7 +36,7 @@ if(isset($error_message)){
 				'<td>'.ucwords(strtolower($item['distr_name'])).'</td>';
         echo '<td><form method="post" action="preferred_districts2_list.php" onsubmit="return confirm(\'Are you sure you want to delete this school?\')">';
         echo '<input type="hidden" value="'.$item['mpd2_id'].'" name="delete"/>';
-        echo '<input type="submit" class="btn btn-outline-danger" value="Delete"/>';
+        echo '<input type="submit" class="btn btn-danger" value="Delete"/>';
         echo '</form>';
         echo '</td></tr>';
 			}
