5f284b767e6ce452a8c4b65f8764b87ea76b9d29
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-14 17:14:46 +0200

    Update reports.php

--- reports.php
+++ reports.php
@@ -3,47 +3,50 @@
 $pageTitle = "Swop Match Handler | Home";
 include("inc/header.php"); ?>
 
-<!DOCTYPE = html>
-<html>
-  <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
-	<title><?php echo $pageTitle ?></title>
-	<link rel = "stylesheet" href = "css/styles.css"> 
-  </head>
-	<div class="form">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php echo $pageTitle ?></title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+	<div class="form" class="text-justify">
 		<h2>Please select a report below</h2>
-		
-		<table class = "table">
+
+        <table class="table >
 			<tr>
-				<td><a href="clients_list.php" target = "_blank" class = "prefButtons">Clients List</a></td>
-        <td><a href="current_schools_list.php" target = "_blank"  class = "prefButtons">Current Schools List</a></td>
-				<td><a href="preferred_provinces_list.php" target = "_blank" class = "prefButtons">Preferred Provinces List</a></td>
-				<td><a href="preferred_districts_list.php" target = "_blank" class = "prefButtons">Preferred Districts1 List</a></td>
+			<td scope="col"></td>
+				<td><a href="clients_list.php" class="btn btn-success target = "_blank"  class = "prefButtons">Clients List</a></td>
+        <td><a href="current_schools_list.php" class="btn btn-success target = "_blank"  class = "prefButtons">Current Schools List</a></td>
+				<td><a href="preferred_provinces_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Provinces List</a></td>
+				<td><a href="preferred_districts_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Districts1 List</a></td>
 			</tr>
 			<tr>
-        <td><a href="preferred_districts2_list.php" target = "_blank" class = "prefButtons">Preferred Districts2 List</a></td>
-				<td><a href="preferred_towns_list.php" target = "_blank" class = "prefButtons">Preferred Towns List</a></td>
-				<td><a href="preferred_locations_list.php" target = "_blank" class = "prefButtons">Preferred Locations1 List</a></td>
-        <td><a href="preferred_locations2_list.php" target = "_blank" class = "prefButtons">Preferred Locations2 List</a></td>
+        <td><a href="preferred_districts2_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Districts2 List</a></td>
+				<td><a href="preferred_towns_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Towns List</a></td>
+				<td><a href="preferred_locations_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Locations1 List</a></td>
+        <td><a href="preferred_locations2_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Locations2 List</a></td>
 			</tr>
       <tr>
-        <td><a href="preferred_locations3_list.php" target = "_blank" class = "prefButtons">Preferred Locations3 List</a></td>
-				<td><a href="preferred_schools_list.php" target = "_blank" class = "prefButtons">Preferred schools1 List</a></td>
-				<td><a href="preferred_schools2_list.php" target = "_blank" class = "prefButtons">Preferred schools2 List</a></td>
-        <td><a href="preferred_schools3_list.php" target = "_blank" class = "prefButtons">Preferred schools3 List</a></td>
+        <td><a href="preferred_locations3_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred Locations3 List</a></td>
+				<td><a href="preferred_schools_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools1 List</a></td>
+				<td><a href="preferred_schools2_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools2 List</a></td>
+        <td><a href="preferred_schools3_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools3 List</a></td>
 			</tr>
        <tr>
-        <td><a href="preferred_schools4_list.php" target = "_blank" class = "prefButtons">Preferred schools4 List</a></td>
-				<td><a href="preferred_schools5_list.php" target = "_blank" class = "prefButtons">Preferred schools5 List</a></td>
-				<td><a href="preferred_schools6_list.php" target = "_blank" class = "prefButtons">Preferred schools6 List</a></td>
-        <td><a href="preferred_schools7_list.php" target = "_blank" class = "prefButtons">Preferred schools7 List</a></td>
+        <td><a href="preferred_schools4_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools4 List</a></td>
+				<td><a href="preferred_schools5_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools5 List</a></td>
+				<td><a href="preferred_schools6_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools6 List</a></td>
+        <td><a href="preferred_schools7_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools7 List</a></td>
 			</tr>
       <tr>
-        <td><a href="preferred_schools8_list.php" target = "_blank" class = "prefButtons">Preferred schools8 List</a></td>
-				<td><a href="preferred_schools9_list.php" target = "_blank" class = "prefButtons">Preferred schools9 List</a></td>
-				<td><a href="preferred_schools10_list.php" target = "_blank" class = "prefButtons">Preferred schools10 List</a></td>
+        <td><a href="preferred_schools8_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools8 List</a></td>
+				<td><a href="preferred_schools9_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools9 List</a></td>
+				<td><a href="preferred_schools10_list.php" class="btn btn-success target = "_blank" class = "prefButtons">Preferred schools10 List</a></td>
         <td></td>
 			</tr>
 		</table>
