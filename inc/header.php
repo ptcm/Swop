c5a2839eaafc9f4a3023a06eeb5fe91823b594c1
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-16 17:24:18 +0200

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,26 +1,27 @@
-<!DOCTYPE = html>
-<html>
-  <head>
-  <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="../csss/main.css">
-    <title><?php $pageTitle ?></title>
-	<link rel = "stylesheet" href = "../css/styles.css"> 
-  </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SwopMatch Handler</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-  <body class = "body">
-    <div class = "section hidden">
-		<header>
-		  <h1 id="sitename">SwopMatch Handler</h1>
-		<!--	<nav class = "menu" id="mainlinks"> -->
-			      <ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="Account_manage.php" target = "_blank">My Account</a></li>
-					<li><a href="suggest.php" target = "_blank">Suggest</a></li>
-					<li><a href="reports.php" target = "_blank">Reports</a></li>
-					<li><a href="#">Contact</a></li>
-				</ul>
-			<!-- </nav> -->
-		</header>
-	</div>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="btn-group btn-group-justified">
+            <a class="navbar-brand"  href="#">SwopMatchHandler</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php" class="text-primary">Home</a></li>
+            <li><a href="Account_manage.php" class="text-primary">My Account</a></li>
+            <li><a href="suggest.php" class="text-primary">Suggest</a></li>
+            <li><a href="reports.php" class="text-primary">Reports</a></li>
+            <li><a href="contact.php" class="text-primary">contact</a></li>
+        </ul>
+    </div>
+</nav>
+
