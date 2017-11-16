5ec46b084a6c480c823a57301e7a419543a2f8ac
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-16 17:32:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,52 +4,37 @@ include("inc/header.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?php echo $pageTitle ?></title>
-
-    <!-- Required meta tags -->
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script type="text/javascript" src="js/script.js"></script>
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" 
-	  integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SwopMatchHandler</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
 </head>
-<body>
+<body id="home" data-spy="scroll" data-target=".navbar" data-offset="100">
 
 <!-- jumbotron -->
-<div class="jumbotron jumbotron-fluid bg-info text-white">
-    <div class="container text-sm-center pt-5">
-        <h1 class="display-2">SwopMatch Handler</h1>
-        <p class="lead">Make life easy with Swop Match Handler! A place where we take care of you!</p>
-        <div class="btn-group mt-4" role="group" aria-label="Callout buttons">
-            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#register">signup for SwopMatchHandler Now</button>
+<div class="card-body text-info container-fluid">
+    <div class="container text-sm-center p-t-3">
+        <h1 class="display-2">SwopMatchHandler</h1>
+        <p class="lead">SwopMatch Handler brings affordable technology to people everywhere to help them achieve their dreams and change the Nation!</p>
 
-        </div>
-    </div>
-</div><!-- /jumbotron -->
-
-<!-- callout button -->
-<!--<button type="button" class="btn btn-outline-info btn-lg d-block mx-auto my-5" data-toggle="modal" data-target="#register">Don't Be left Out, Signup Now</button>
-
-<!-- signup form -->
-<hr>
-<div class="row py-4 text-muted text-center">
-    <div class="col-md col-xl-5">
-        <p><strong>About Swop Match Handler</strong></p>
-        <p class="font-weight-bold">SwopMatch Handler brings affordable technology to people everywhere to help them achieve their dreams and change the Nation.</p>
-    </div>
-    <div class="col-md col-xl-5 ml-auto">
-        <p><strong>signup for updates on Swop Match Handler</strong></p>
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Email">
-            <span class="input-group-btn">
-			        <button class="btn btn-primary" type="button">Sign up</button>
-			      </span>
+        <div class="btn-group m-t-2" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#register">Register Now</button>
+            <a class="btn btn-secondary btn-lg" href="#speakers">See Speakers</a>
         </div>
     </div>
 </div>
-<hr><!-- /signup form -->
+<!-- /jumbotron -->
 
-
+        <div class="col-md-6 col-xl-5 col-xl-offset-2">
+            <p><strong>signup for updates on Swop Match Handler</strong></p>
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Email">
+                <span class="input-group-btn">
+			        <button class="btn btn-primary" type="button">Sign up</button>
+			      </span>
+            </div>
+        </div>
+    </div>
+    <hr><!-- /signup form -->
