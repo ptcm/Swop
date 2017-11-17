0189adf0b31c551141db1ea4478ee21ad1293f0b
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-17 07:21:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,60 @@
-<?php
-$pageTitle = "Swop Match Handler | Home";
-include("inc/header.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>SwopMatchHandler</title>
+    <title>SwopMatch Handler</title>
     <!-- bootstrap css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
 </head>
 <body id="home" data-spy="scroll" data-target=".navbar" data-offset="100">
 
+<!-- navbar -->
+<nav class="navbar navbar-dark bg-primary navbar-fixed-top">
+    <div class="container">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Account_manage.php" class="text-primary">My Account</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="suggest.php" class="text-primary">Suggest</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="reports.php" class="text-primary">Reports</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php" class="text-primary">contact</a>
+            </li>
+
+        </ul>
+        <a class="navbar-brand pull-sm-right m-r-0 hidden-xs-down" href="pchizema@gmail.com">Presented by P.Chizema</a>
+    </div>
+</nav><!-- /navbar -->
+
 <!-- jumbotron -->
-<div class="card-body text-info container-fluid">
+<div class="jumbotron jumbotron-fluid bg-warning">
     <div class="container text-sm-center p-t-3">
         <h1 class="display-2">SwopMatchHandler</h1>
-        <p class="lead">SwopMatch Handler brings affordable technology to people everywhere to help them achieve their dreams and change the Nation!</p>
+        <p class="lead">SwopMatch Handler brings affordable Swops / Transfers technology to people in all the organisation everywhere in  the Nation!</p>
 
         <div class="btn-group m-t-2" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#register">Register Now</button>
-            <a class="btn btn-secondary btn-lg" href="#speakers">See Speakers</a>
-        </div>
-    </div>
-</div>
-<!-- /jumbotron -->
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#register">Register Now For Swops / Transfers</button>
 
-        <div class="col-md-6 col-xl-5 col-xl-offset-2">
-            <p><strong>signup for updates on Swop Match Handler</strong></p>
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Email">
-                <span class="input-group-btn">
+        </div>
+
+<div class="jumbotron jumbotron-fluid bg-success">
+    <div class="container text-sm-center p-t-3">
+        <p><strong>signup for updates on Swop Match Handler</strong></p>
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Email/ UserName or phone number">
+            <span class="input-group-btn">
 			        <button class="btn btn-primary" type="button">Sign up</button>
 			      </span>
-            </div>
         </div>
     </div>
-    <hr><!-- /signup form -->
+    <?php include("inc/footer.php"); ?>
+
