7e02e1c1c90555a9201b3e687278b81d35109557
Lovejoymaluwa <maluwalovejoy2@gmail.com>
2017-11-17 07:18:34 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,41 +1,33 @@
-<?php
-$pageTitle = "| Home";
-include("inc/header.php"); ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>SwopMatch Handler</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title><?php echo $pageTitle ?></title>
 
-    <!-- Required meta tags -->
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script type="text/javascript" src="js/script.js"></script>
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
-          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-</head>
 <!--contact-->
-<div class="jumbotron alert-danger clearfix class="border border-primary">
-    <div class="card-header">Contact</div>
-    <div class="container w-50 p-3 pt-5">
-
+    <div class="jumbotron jumbotron-fluid bg-info">
+        <div class="container p-3 mb-2 bg-success text-white">
+        <h4 class="card-title">Contact</h4>
         <p class="card-text">
-        <p>for more info you may contact!!</p>
         <p><strong>Email:</strong><a href="mailto:pchizema@gmail.com>?subject=if you have issues I  will be to assist you!">pchizema@gmail.com</a></p>
         <p><strong>Phone:</strong>+263 712549887</p>
         <p><strong>Email:</strong> <a href="mailto:maluwalovejoy2@gmail.com?subject=Hi There, you are free to ask anything!">maluwalovejoy2@gmail.com</a></p>
         <p><strong>Phone:</strong>+263 778846005</p>
-        <footer>
-            <p><small>&copy; SwopMatch Handler</small></p>
-        </footer></p>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-        </body>
-</html>
-<?php include("inc/footer.php"); ?>
+        <p><strong>Address:</strong></p>
+        <address>
+            Education Solution<br>
+            23 Harare Drive<br>
+            Zimbabwe<br>
+        </address>
+<?php
+include 'inc/header.php';
+include 'inc/functions.php';
